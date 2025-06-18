7e62552ba0c928af89a82929fd13595e47a3ed37
GustasVinciauskas <kimukasgerve@gmail.com>
2025-06-18 23:02:17 +0300

    Add files via upload

---
+++ website.js
@@ -0,0 +1,17 @@
+const myDiv = document.getElementById("myDiv");
+
+const myButton = document.createElement("button");
+myButton.innerText = "Login";
+myButton.className = "Loginbttn";
+
+myButton.onclick = function () {
+window.open("https://www.youtube.com","_blank");
+};
+
+myDiv.appendChild(myButton);
+
+const zinute = document.getElementById("zinute");
+zinute.onclick = function () {
+    window.open("https://www.youtube.com/watch?v=CGvE-eimyus", "_blank");
+};
+zinute.appendChild(mygtukas)
